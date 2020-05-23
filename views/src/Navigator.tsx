1a7cffb5284e236aa0a5e1eca8faf606c0b8a14d
muskankhedia <muskan.khedia2000@gmail.com>
2020-05-23 23:24:09 +0530

    Revert "added basic form"
    This reverts commit 4789d561e623ff7308f9c55baa9fbd66ef561a9a.

--- views/src/Navigator.tsx
+++ views/src/Navigator.tsx
@@ -3,14 +3,12 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from './components/login/Login';
 import Profiles from './components/profiles/Profiles';
-import CreateProfile from './components/createProfile/CreateProfile';
 
 const Navigator: FC<{}> = () => (
   <Router>
     <Switch>
       <Route exact={true} path="/" component={Login}  />
       <Route exact={true} path="/profiles" component={Profiles}  />
-      <Route exact={true} path="/createprofile" component={CreateProfile}  />
     </Switch>
   </Router>
 );
