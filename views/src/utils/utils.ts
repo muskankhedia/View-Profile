b8352dcc1d488297df1ffab114881548231f69bc
muskankhedia <muskan.khedia2000@gmail.com>
2020-05-24 13:58:44 +0530

    changed CLIENT

--- views/src/utils/utils.ts
+++ views/src/utils/utils.ts
@@ -1,3 +1,3 @@
 export const HOST = 'https://view-profile-app.herokuapp.com';
-export const CLIENT = 'http://localhost:3000';
+export const CLIENT = 'https://serene-joliot-1bc072.netlify.app';
 // export const HOST = 'http://localhost:5000';
